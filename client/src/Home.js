65cfb75f8dd863ee56fb1be1c2ad8bf9281ed5bc
Karan Bhatia <userkaranb@gmail.com>
2017-08-31 18:55:58 -0400

    Setting up iterative divs
    todo:
      - set up function to return div based on given product data
      - Break that out into separate functions

--- client/src/Home.js
+++ client/src/Home.js
@@ -1,15 +1,31 @@
 import React, { Component } from 'react';
 import Request from 'superagent';
 class Home extends Component {
+  constructor(){
+    super()
+    this.state = {products: []}
+  }
   
   componentWillMount(){
     var url = "http://localhost:5000/products"
-    Request.get(url).then((response) => console.log(response));
+    Request.get(url).then((response) => {
+        this.setState({
+            products: response.body
+        })
+    });
   }
 
   render() {
+    let content;
+    content = Object.keys(this.state.products).map(
+        function(key) {
+            return (<div key={key} id={key}/>)
+        })
+
     return (
-      <form>Hi</form>
+        <div>
+        {content}
+       </div>
     );
   }
 }
