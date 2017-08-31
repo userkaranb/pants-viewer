a72c648cd32d47b30aca553cb4efcb07ae878e4f
Karan Bhatia <userkaranb@gmail.com>
2017-08-31 16:26:24 -0400

    Modified data access for sqlite changes

--- server/data/DataAccess.py
+++ server/data/DataAccess.py
@@ -1,25 +1,20 @@
-import MySQLdb
+import sqlite3
+
 class DataAccess(object):
     def __init__(self):
-        self.mydb = MySQLdb.connect(
-            host='localhost',
-            user='root',
-            passwd='pants123',
-            db='Pants'
-        )
+        conn = sqlite3.connect('data/pants.db')
+        self.cursor = conn.cursor()
 
     def query(self):
-        q = 'SELECT * FROM pants.tbl_inventory AS i '\
-        'INNER JOIN pants.tbl_products AS p on i.product_id = p.product_id;'
+        q = '''SELECT * FROM inventory AS i INNER JOIN products AS p on i.product_id = p.product_id;'''
         return q
 
     def get_all_product_rows(self):
         result = []
-        cursor = self.mydb.cursor()
-        cursor.execute(self.query())
-        field_names = [i[0] for i in cursor.description]
+        self.cursor.execute(self.query())
+        field_names = [i[0] for i in self.cursor.description]
         result.append(field_names)
-        for row in cursor:
+        for row in self.cursor:
             result.append(row)
         return result
 
